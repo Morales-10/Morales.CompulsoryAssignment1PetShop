2b33b6f89ac0dedd2e891b7392046f0760557db0
Morales-10 <anna450s@easv365.dk>
2021-09-09 22:42:35 +0200

    prøve

---
+++ Morales.CompulsoryPetShop.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Morales.CompulsoryPetShop.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
